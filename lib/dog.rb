efef3c41e769e100fadfea202efbbcf2019f83cf
sheridan <sheridanmaustad@gmail.com>
2022-03-15 00:15:32 -0600

    commit tree 4d01a9e912679bd5d284df818b7eb28f1cbf40f5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+attr_accessor :name, :breed, :id
 
+def initialize(name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
+end
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dog (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+        SQL
+        DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+
+        end
 end
